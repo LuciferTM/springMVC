32ba6214258739816ae005f89c86e0192f52f1ed
LuciferTM <roy_qian@gmail.com>
2017-05-29 14:22:13 +0800

    resultmap映射

--- src/main/java/example/mybatis/resultmap/ResultMapTest.java
+++ src/main/java/example/mybatis/resultmap/ResultMapTest.java
@@ -1,28 +1,28 @@
 package example.mybatis.resultmap;
 
 import example.mybatis.util.TestSqlSessionFactory;
+import example.pojo.mybatis.User;
 import org.apache.ibatis.session.SqlSession;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class SelectMapTest {
+public class ResultMapTest {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // 获得Session实例
         SqlSession session = TestSqlSessionFactory.getSqlSession();
         // 查询TB_USER表所有数据返回List集合,集合中的每个元素都是一个Map
-        List<Map<String,Object>> list
-                = session.selectList("example.mapper.UserMapper.selectUser1");
+        List<User> user_list
+                = session.selectList("example.mapper.UserMapper.selectUser2");
         // 遍历List集合，打印每一个Map对象
-        for(Map<String,Object> row : list){
-            System.out.println(row);
+        for(User user : user_list){
+            System.out.println(user);
         }
         // 提交事务
         session.commit();
         // 关闭Session
         session.close();
+
     }
 
 }
